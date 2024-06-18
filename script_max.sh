4ab78de7326fa50264236f45c1aace7d376af028
mattiamencagli <mattiamencagli@hotmail.it>
2024-06-18 09:12:47 +0200

    ..

--- script_max.sh
+++ script_max.sh
@@ -5,7 +5,7 @@ rm maxs.dat
 for i in {0..5000..10}
 do
 	echo -n "$i " >> maxs.dat 
-	for dir in 128 64 32 16 8 4 2
+	for dir in 128 64 32 16 8 4
 	do
 		head -n 1 ./lambda${dir}/ux${i}.dat | rs -T | awk 'BEGIN{max=0}{if($1>max) max=$1}END{printf "%g ",max}' >> maxs.dat
 	done
